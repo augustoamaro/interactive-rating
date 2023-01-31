8001904456b3c38eea8efafe0855f8853bc69a9e
Leônidas Augusto Amaro <augustoamaro@icloud.com>
2023-01-31 13:48:14 -0300

    Finished project

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-// const circles = document.querySelectorAll('span');
-// for (let circle of circles) {
-//     circle.onclick = function () {
-//         
-//         const value = circle.textContent;
-//         console.log(value)
-//     }
-// }
-
 
 const spans = document.querySelectorAll('span');
+let clickedSpans = [];
 
 for (let span of spans) {
     span.addEventListener('click', function () {
         span.style.background = '#FC7614'
         span.style.color = '#FFFFFF'
-        // for (let otherSpan of spans) {
-        //     otherSpan.classList.remove('selected');
-        // }
-        // this.classList.add('selected');
-
+        clickedSpans.push(spans)
     });
 }
 
 const handleSubmit = () => {
+    const selection = document.querySelector('[selection]');
+    console.log(selection)
 
     const cardContent = document.querySelector('[card_content]')
     cardContent.style.display = 'none';
 
     const cardContentFinal = document.querySelector('[card_final]');
     cardContentFinal.style.display = 'flex';
-    console.log(`You selected ${this.innerText} of 5`);
+    selection.innerHTML = `You selected ${clickedSpans.length} of 5`
 }
