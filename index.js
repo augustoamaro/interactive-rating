dcf700eda5685141c0cf824e58871c46325ff46e
Leônidas Augusto Amaro <augustoamaro@icloud.com>
2023-02-21 21:06:56 -0300

    Fixed JS value

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
+const inputs = document.querySelectorAll('input[type=radio]');
+const selection = document.querySelector('[selection]');
 
-const spans = document.querySelectorAll('span');
-let clickedSpans = [];
-
-for (let span of spans) {
-    span.addEventListener('click', function () {
-        span.style.background = '#FC7614'
-        span.style.color = '#FFFFFF'
-        clickedSpans.push(spans)
+inputs.forEach(input => {
+    input.addEventListener('click', () => {
+        selection.innerHTML = `You selected ${input.value} out of 5`
     });
-}
+});
 
 const handleSubmit = () => {
-    const selection = document.querySelector('[selection]');
     const cardContent = document.querySelector('[card_content]')
     const cardContentFinal = document.querySelector('[card_final]');
 
     cardContent.style.display = 'none';
     cardContentFinal.style.display = 'flex';
-    selection.innerHTML = `You selected ${clickedSpans.length} out of 5`
 }
